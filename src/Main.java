b9004ebdff4429f28d61ff878099f39d1343fc25
104021056 <104021056@live.asia.edu.tw>
2018-09-26 14:33:55 +0800

    test1

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("輸入一攝氏溫度");
         Scanner scn = new Scanner(System.in);
-        float C = scn.nextFloat();
-        float F = C*9/5+32;
-        System.out.println("華氏溫度"+ F);
+        char ch = scn.next().charAt(0);
+        System.out.println((int)ch);
     }
 }
